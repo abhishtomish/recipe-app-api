e0fff747c61bad672f4477ebb50588c55ea0e6ab
abhishtomish <abshek786@gmail.com>
2020-07-12 17:25:49 +0530

    Assertion issue fixed

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -72,8 +72,8 @@ class PublicUserApiTest(TestCase):
         create_user(**payload)
         res = self.client.post(TOKEN_URL, payload)
 
-        self.assertNotIn('token', res.data)
-        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_create_token_invalid_credentials(self):
         """Test that token is not created if invalid credentials are given"""
